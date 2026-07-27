f013899cea3a49ab0c185c05d211e5cd1c3409b3
GuptaCosmeticShop0Messhoseller <anshraj33668@gmail.com>
2026-07-27 21:37:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAKZKrCxiIzthZKkXrkAY46mTZNJDw4CJ0",
+  authDomain: "gupta-cosmetic-shop-and-messho.firebaseapp.com",
+  databaseURL: "https://gupta-cosmetic-shop-and-messho-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gupta-cosmetic-shop-and-messho",
+  storageBucket: "gupta-cosmetic-shop-and-messho.firebasestorage.app",
+  messagingSenderId: "1063721196033",
+  appId: "1:1063721196033:web:d7b1b9d0ea1a5e28c27e00"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.saveOrderToFirebase = async function(order) {
+  try {
+    await push(ref(db, "orders"), {
+      ...order,
+      status: "Pending",
+      createdAt: new Date().toISOString()
+    });
+    alert("✅ Order Saved Successfully");
+  } catch (error) {
+    alert("❌ " + error.message);
+  }
+};
